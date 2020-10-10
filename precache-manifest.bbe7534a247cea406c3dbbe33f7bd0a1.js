ad32fdff7da291a51ac12523c4469f6eaa9b409e
Muhammad Idrees <idreesiqbal104@gmail.com>
2020-10-10 19:06:04 +0500

    Updates

---
+++ precache-manifest.bbe7534a247cea406c3dbbe33f7bd0a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37f9443c0f1847245b50e60716d81e0",
+    "url": "/dominus-freight-app/index.html"
+  },
+  {
+    "revision": "d007c92f85657d016bbb",
+    "url": "/dominus-freight-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "47b93977a43d84b01fdb",
+    "url": "/dominus-freight-app/static/css/main.a9059ebb.chunk.css"
+  },
+  {
+    "revision": "d007c92f85657d016bbb",
+    "url": "/dominus-freight-app/static/js/2.6b0b1785.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dominus-freight-app/static/js/2.6b0b1785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b93977a43d84b01fdb",
+    "url": "/dominus-freight-app/static/js/main.7b598a3c.chunk.js"
+  },
+  {
+    "revision": "f2508a1f7877472999d3",
+    "url": "/dominus-freight-app/static/js/runtime-main.437c55bd.js"
+  },
+  {
+    "revision": "650a2a150780d6d9c8bef273e3da5bfc",
+    "url": "/dominus-freight-app/static/media/map.650a2a15.png"
+  }
+]);
